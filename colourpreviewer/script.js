d411829437d8b34c021a19dd84dd671715b31de9
thomas <thomasdpb@gmail.com>
2017-09-26 17:54:21 +0100

    octarine

--- colourpreviewer/script.js
+++ colourpreviewer/script.js
@@ -1,4 +1,8 @@
 function change() {
     let colour = document.getElementById("colour_text").value;
+    if(colour.toLowerCase() == "#hhhhhh") {
+        alert("ahh.... the colour of magic... unfortunatly your web browser isn't quite capable of showing that...");
+        return;
+    }
     document.body.style.background = colour;
 }
