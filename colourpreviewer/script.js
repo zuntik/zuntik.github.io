32212cbef4fbb738766c50f806fae2c11078c907
Thomas Berry <thomasdpb@gmail.com>
2019-09-26 16:45:33 +0100

    fiona

--- colourpreviewer/script.js
+++
@@ -1,8 +0,0 @@
-function change() {
-    let colour = document.getElementById("colour_text").value;
-    if(colour.toLowerCase() == "#hhhhhh") {
-        alert("ahh.... the colour of magic... unfortunatly your web browser isn't quite capable of showing that...");
-        return;
-    }
-    document.body.style.background = colour;
-}
